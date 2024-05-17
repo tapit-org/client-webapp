0cdae25534e75d3e3841389c0f125483a87fca29
Veer <veer.vs19@gmail.com>
2024-05-17 12:39:05 +0530

    Added Social Share Icons

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useMemo } from "react";
+import { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import NcImage from "shared/NcImage/NcImage";
 import Prices from "components/Prices";
@@ -6,22 +6,27 @@ import { StarIcon } from "@heroicons/react/24/solid";
 import ButtonPrimary from "shared/Button/ButtonPrimary";
 import ButtonSecondary from "shared/Button/ButtonSecondary";
 import toast from "react-hot-toast";
-import { Transition } from "@headlessui/react";
 import ProductTag from "components/ProductTag";
 import { ProductListItemInterface } from "interfaces/product.interface";
-import { AddShoppingCartOutlined, ShareOutlined } from "@mui/icons-material";
+import {
+	AddShoppingCartOutlined,
+	Close,
+	ShareOutlined,
+} from "@mui/icons-material";
 import { addCartItem } from "store/reducers/cart";
 import { dispatch } from "store";
 import NotifyAddToCart from "components/NotifyAddToCart";
-import { useSelector } from "react-redux";
-import { CartItemInterface } from "interfaces/cart.interface";
-
+import { FacebookShare, TwitterShare, WhatsappShare } from "react-share-kit";
+import { IconButton } from "@mui/material";
 export interface ProductCardProps {
 	product: ProductListItemInterface;
 }
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
 	const productLink = "/product/" + product.id;
+	const shareLink = `https://tap-it.in${productLink}`;
+	const shareText = "Hey! Checkout this product by Tap-It";
+	const [showShareIcons, setShowShareIcons] = useState(false);
 	const handleAddToCart = () => {
 		const itemToAdd = {
 			id: product.id,
@@ -43,77 +48,14 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
 		});
 	};
 
-	const renderProductCartOnNotify = () => {
-		return (
-			<div className="flex">
-				<div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-xl bg-slate-100">
-					<img
-						src={product.thumbnail}
-						alt={product.name}
-						className="h-full w-full object-cover object-center"
-					/>
-				</div>
-
-				<div className="ml-4 flex flex-1 flex-col">
-					<div>
-						<div className="flex justify-between ">
-							<div>
-								<h4 className="text-base font-medium ">
-									{product.name}
-								</h4>
-							</div>
-							<Prices price={product.price} className="mt-0.5" />
-						</div>
-					</div>
-					<div className="flex flex-1 items-end justify-between text-sm">
-						<p className="text-gray-500 dark:text-slate-400">
-							Qty 1
-						</p>
-
-						<div className="flex">
-							<Link
-								to={"/cart"}
-								className="font-medium text-primary-6000 dark:text-primary-500 "
-							>
-								View cart
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	};
-
-	const renderGroupButtons = () => {
-		return (
-			<div className="absolute bottom-0 group-hover:bottom-4 inset-x-1 flex justify-center opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all">
-				<ButtonPrimary
-					className="shadow-lg"
-					fontSize="text-xs"
-					sizeClass="py-2 px-4"
-					onClick={() => handleAddToCart()}
-				>
-					<AddShoppingCartOutlined fontSize="small" />
-					<span className="ml-2">Add to Cart</span>
-				</ButtonPrimary>
-				<ButtonSecondary
-					className="ml-1.5 bg-white hover:!bg-gray-100 hover:text-slate-900 transition-colors shadow-lg"
-					fontSize="text-xs"
-					sizeClass="py-2 px-4"
-					onClick={() => {}}
-				>
-					<ShareOutlined fontSize="small" />
-					<span className="ml-2">Share</span>
-				</ButtonSecondary>
-			</div>
-		);
-	};
-
 	return (
 		<>
 			<div
 				className="nc-ProductCard relative flex flex-col bg-transparent"
 				data-nc-id="ProductCard"
+				onMouseLeave={() =>
+					setTimeout(() => setShowShareIcons(false), 100)
+				}
 			>
 				<Link to={productLink} className="absolute inset-0"></Link>
 
@@ -126,7 +68,78 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
 						/>
 					</Link>
 					<ProductTag key={product.tag} tag={product.tag} />
-					{renderGroupButtons()}
+					<div className="absolute bottom-0 group-hover:bottom-4 inset-x-1 flex justify-center opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all">
+						{showShareIcons ? (
+							<div className="flex">
+								<FacebookShare
+									url={shareLink}
+									quote={shareText}
+									size={40}
+									round
+									style={{
+										marginLeft: 3,
+										marginRight: 3,
+									}}
+								/>
+								<TwitterShare
+									url={shareLink}
+									title={shareText}
+									size={40}
+									round
+									style={{
+										marginLeft: 3,
+										marginRight: 3,
+									}}
+								/>
+								<WhatsappShare
+									url={shareLink}
+									title={shareText}
+									separator=":: "
+									size={40}
+									round
+									style={{
+										marginLeft: 3,
+										marginRight: 3,
+									}}
+								/>
+								<IconButton
+									onClick={() => setShowShareIcons(false)}
+									sx={{
+										background: "white",
+										marginLeft: 0.5,
+										marginRight: 0.5,
+										":hover": {
+											background: "black",
+											color: "white",
+										},
+									}}
+								>
+									<Close />
+								</IconButton>
+							</div>
+						) : (
+							<div>
+								<ButtonPrimary
+									className="shadow-lg"
+									fontSize="text-xs"
+									sizeClass="py-2 px-4"
+									onClick={() => handleAddToCart()}
+								>
+									<AddShoppingCartOutlined fontSize="small" />
+									<span className="ml-2">Add to Cart</span>
+								</ButtonPrimary>
+								<ButtonSecondary
+									className="ml-1.5 bg-white hover:!bg-gray-100 hover:text-slate-900 transition-colors shadow-lg"
+									fontSize="text-xs"
+									sizeClass="py-2 px-4"
+									onClick={() => setShowShareIcons(true)}
+								>
+									<ShareOutlined fontSize="small" />
+									<span className="ml-2">Share</span>
+								</ButtonSecondary>
+							</div>
+						)}
+					</div>
 				</div>
 
 				<div className="space-y-4 px-2.5 pt-5 pb-2.5">

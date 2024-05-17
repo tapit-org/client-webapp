7b2bcdb0d087d138f2ab8d2ee8e4e7249f4e8c7a
Veer <veer.vs19@gmail.com>
2024-05-17 12:43:46 +0530

    Added default footer

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -14,6 +14,7 @@ import { User, deleteUser } from "@firebase/auth";
 import { UserInterface } from "interfaces/user.interface";
 import OverlayLoader from "components/OverlayLoader";
 import { auth } from "firebaseConfig";
+import Footer from "shared/Footer/Footer";
 
 // ==============================|| MINIMAL LAYOUT ||============================== //
 
@@ -87,6 +88,7 @@ const Layout = ({ restricted = false, redirect = false }) => {
 				<ScrollToTop />
 				<Header />
 				<Outlet />
+				<Footer />
 			</>
 		);
 	}
